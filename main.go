e85217a03056b3c90f9d51a84ba6d058d1329836
zhangrongdong <zhangrongdong@zeek.one>
2023-07-12 11:08:55 +0800

    添加了中间件和监听端口

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"snake/middlewares"
+)
 
 func main() {
-	// Call the function
-	fmt.Println("Hello, World!")
+	r := gin.Default()
+
+	r.Use(middlewares.Cors())
+
+	r.StaticFS("/web", http.Dir("./web"))
+
+	// 监听80端口
+	r.Run("0.0.0.0:80")
+
 }
